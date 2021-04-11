dbbd158278b1886a2ef18c34d5e077c5b836fdd5
GabrielIthurralde <73235232+GabrielIthurralde@users.noreply.github.com>
2021-04-11 04:23:07 -0300

    GI agregué actividad bienvenida, actividad login usando plantilla, falta unirlas, darle estética, probarlas

---
+++ app/src/main/java/app2you/probateapp/Bienvenida.java
@@ -0,0 +1,14 @@
+package app2you.probateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bienvenida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida);
+    }
+}
