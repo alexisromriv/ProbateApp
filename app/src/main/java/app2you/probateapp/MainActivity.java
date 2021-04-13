fda1a70a8b62c33708f80c2a2adc874ae0c63f70
Pabla <pagorio@gmail.com>
2021-04-12 21:53:20 -0300

    listado materias

--- app/src/main/java/app2you/probateapp/MainActivity.java
+++ app/src/main/java/app2you/probateapp/MainActivity.java
@@ -5,16 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 
 import app2you.probateapp.ui.login.LoginActivity;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+
+
     public static final String EXTRA_MESSAGE = "com.app2you.probateapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     /** Called when the user taps the Send button */
