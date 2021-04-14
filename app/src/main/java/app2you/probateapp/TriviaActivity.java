283c5edba25dba73ff4a60ed5b1571cbe62112fa
Alexis Romero <romeroalexis1994@gmail.com>
2021-04-13 21:27:20 -0300

    TriviaActivity

--- app/src/main/java/app2you/probateapp/TriviaActivity.java
+++ app/src/main/java/app2you/probateapp/TriviaActivity.java
@@ -1,15 +1,22 @@
 package app2you.probateapp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 import app2you.probateapp.controladores.Autenticacion;
 import app2you.probateapp.controladores.Trivia;
 import app2you.probateapp.entidades.Pregunta;
+import app2you.probateapp.entidades.Respuesta;
 import app2you.probateapp.entidades.Tema;
 import app2you.probateapp.entidades.Usuario;
 
@@ -22,7 +29,10 @@ public class TriviaActivity extends AppCompatActivity implements View.OnClickLis
     private TextView tvRespuesta3;
 
     private Trivia trivia;
+    private Tema tema;
     private Pregunta pregunta;
+    private TextView acertTextView;
+
 
 
     @Override
@@ -42,32 +52,81 @@ public class TriviaActivity extends AppCompatActivity implements View.OnClickLis
 
         try {
             Usuario usr = new Autenticacion().login("alexis", "123");
-            Tema tema = usr.getCurso().getMaterias().get(0).getTemas().get(0);
+            tema = usr.getCurso().getMaterias().get(0).getTemas().get(0);
 
             trivia = new Trivia(tema);
-            pregunta = trivia.siguiente();
+            init();
 
-            tvTema.setText(tema.getNombre());
-            tvPregunta.setText(pregunta.getTitulo());
-            tvRespuesta1.setText(pregunta.getRespuestas().get(0).getTitulo());
-            tvRespuesta2.setText(pregunta.getRespuestas().get(1).getTitulo());
-            tvRespuesta3.setText(pregunta.getRespuestas().get(2).getTitulo());
-        }catch (Exception ex){
+        } catch (Exception ex) {
 
         }
 
+    }
+
+    private void init() {
+        pregunta = trivia.siguiente();
+        tvTema.setText(tema.getNombre());
+        tvPregunta.setText(pregunta.getTitulo());
+        tvRespuesta1.setBackgroundColor(Color.parseColor("#ffffff"));
+        tvRespuesta2.setBackgroundColor(Color.parseColor("#ffffff"));
+        tvRespuesta3.setBackgroundColor(Color.parseColor("#ffffff"));
+        tvRespuesta1.setText(pregunta.getRespuestas().get(0).getTitulo());
+        tvRespuesta2.setText(pregunta.getRespuestas().get(1).getTitulo());
+        tvRespuesta3.setText(pregunta.getRespuestas().get(2).getTitulo());
+        tvRespuesta1.setAlpha(1);
+        tvRespuesta2.setAlpha(1);
+        tvRespuesta3.setAlpha(1);
+        if (pregunta.getRespuestas().get(0).isCorrecta()){
+            acertTextView = tvRespuesta1;
+        }
+        if (pregunta.getRespuestas().get(1).isCorrecta()){
+            acertTextView = tvRespuesta2;
+        }
+        if (pregunta.getRespuestas().get(2).isCorrecta()){
+            acertTextView = tvRespuesta3;
+        }
     }
 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+
+        tvRespuesta1.setAlpha(.5f);
+        tvRespuesta2.setAlpha(.5f);
+        tvRespuesta3.setAlpha(.5f);
+        v.setAlpha(1);
+
+        Respuesta respuestaSeleccionada = null;
+        switch (v.getId()) {
             case R.id.tvRespuesta1:
-                Toast.makeText(this, trivia.responder(pregunta.getRespuestas().get(0)) ? "correcta" : "incorrecta", Toast.LENGTH_SHORT).show();
+                respuestaSeleccionada = this.pregunta.getRespuestas().get(0);
+                break;
+
             case R.id.tvRespuesta2:
-                Toast.makeText(this, trivia.responder(pregunta.getRespuestas().get(1)) ? "correcta" : "incorrecta", Toast.LENGTH_SHORT).show();
+                respuestaSeleccionada = this.pregunta.getRespuestas().get(1);
+                break;
+
             case R.id.tvRespuesta3:
-                Toast.makeText(this, trivia.responder(pregunta.getRespuestas().get(2)) ? "correcta" : "incorrecta", Toast.LENGTH_SHORT).show();
+                respuestaSeleccionada = this.pregunta.getRespuestas().get(2);
+                break;
         }
+
+        acertTextView.setBackgroundColor(Color.parseColor("#28A744"));
+
+        if (trivia.responder(respuestaSeleccionada) ) {
+            Toast.makeText(this, "correcta", Toast.LENGTH_SHORT).show();
+        } else {
+            v.setBackgroundColor(Color.parseColor("#D43341"));
+            Toast.makeText(this, "incorrecta", Toast.LENGTH_SHORT).show();
+        }
+
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                TriviaActivity.this.init();
+            }
+        }, 3000);
     }
+
 }
+
