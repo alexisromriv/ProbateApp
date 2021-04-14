97c52ef4d1fa716c760857df61f76e4cfacba711
Pabla <pagorio@gmail.com>
2021-04-14 17:16:55 -0300

    listo

--- app/src/main/java/app2you/probateapp/Registro.java
+++ app/src/main/java/app2you/probateapp/Registro.java
@@ -3,12 +3,32 @@ package app2you.probateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Registro extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.institutos, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        //spinner.setOnItemSelectedListener(this);
+
+        Spinner spinner1 = findViewById(R.id.spinner3);
+
+        ArrayAdapter<CharSequence>adapter1=ArrayAdapter.createFromResource(this,R.array.cursos, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+
+
     }
+
 }
