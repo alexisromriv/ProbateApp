31e843747d783ec40779bbb1c68259374c968225
GabrielIthurralde <73235232+GabrielIthurralde@users.noreply.github.com>
2021-04-14 04:21:45 -0300

    GI mejoré  bienvenida (main), deberían haber quedado bien las ubicaciones de la imagen, el mensaje de bienvenida y el botón de ingresar cambié campo binevenida de edit text a  textview Uní botón de ingresar con vista de login (la que es para loguearse o registrarse), falta agregarle la foro, ver la estética, y agregar el "olvidó su contraseña?"

--- app/src/main/java/app2you/probateapp/MainActivity.java
+++ app/src/main/java/app2you/probateapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -20,24 +21,34 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    public static final String EXTRA_MESSAGE = "com.app2you.probateapp.MESSAGE";
+    //public static final String EXTRA_MESSAGE = "com.app2you.probateapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonIngreso = findViewById(R.id.buttonIngreso);
+        buttonIngreso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), LoginActivity.class);
+                startActivityForResult(intent, 0);
+            }
 
+        });
     }
+
+
+
 
     /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+   // public void sendMessage(View view) {
+      //  Intent intent = new Intent(this, LoginActivity.class);
+      //  EditText editText = (EditText) findViewById(R.id.editText);
+       // String message = editText.getText().toString();
+      //  intent.putExtra(EXTRA_MESSAGE, message);
+      //  startActivity(intent);
         // Do something in response to button
-    }
 
 
 }
