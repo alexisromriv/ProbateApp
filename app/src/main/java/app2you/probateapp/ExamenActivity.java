987fec3d728c98f7ffe34581e7ae25e3e432e38c
Alexis Romero <romeroalexis1994@gmail.com>
2021-04-22 00:06:33 -0300

    Timer examen

--- app/src/main/java/app2you/probateapp/ExamenActivity.java
+++ app/src/main/java/app2you/probateapp/ExamenActivity.java
@@ -2,7 +2,9 @@ package app2you.probateapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Chronometer;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -13,6 +15,7 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
 
 import app2you.probateapp.controladores.Autenticacion;
 import app2you.probateapp.controladores.Examen;
@@ -24,20 +27,23 @@ import app2you.probateapp.entidades.Tema;
 import app2you.probateapp.entidades.Usuario;
 
 
-public class ExamenActivity extends AppCompatActivity  {
+public class ExamenActivity extends AppCompatActivity {
     Examen examen;
     Materia materia;
     private List<RespuestaAlumno> respuestas;
     int preguntaIndex = 0;
     int temaIndex = 0;
+    int preguntasLength = 0;
 
     private TextView tvPregunta;
     private TextView tvMateriaExamen;
     private TextView tvTema;
+    private TextView tvStepExamen;
     private RadioGroup rgRespuestasExamen;
     private RadioButton rbRespuestaExamen1;
     private RadioButton rbRespuestaExamen2;
     private RadioButton rbRespuestaExamen3;
+    private Chronometer chrExamen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,28 +53,39 @@ public class ExamenActivity extends AppCompatActivity  {
         tvPregunta = findViewById(R.id.tvPreguntaExmen);
         tvTema = findViewById(R.id.tvTemaExamen);
         tvMateriaExamen = (TextView) findViewById(R.id.tvMateriaExamen);
+        tvStepExamen = (TextView) findViewById(R.id.tvStepExamen);
         rgRespuestasExamen = (RadioGroup) findViewById(R.id.rgRespuestasExamen);
         rbRespuestaExamen1 = (RadioButton) findViewById(R.id.rbRespuestaExamen1);
         rbRespuestaExamen2 = (RadioButton) findViewById(R.id.rbRespuestaExamen2);
         rbRespuestaExamen3 = (RadioButton) findViewById(R.id.rbRespuestaExamen3);
+        chrExamen = (Chronometer) findViewById(R.id.chrExamen);
+
 
         try {
             Intent intent = getIntent();
 
             materia = (Materia) intent.getSerializableExtra("materia");
             tvMateriaExamen.setText(materia.getNombre());
+
             respuestas = new ArrayList<>();
             for (Tema t : materia.getTemas()) {
                 for (Pregunta p : t.getPreguntas()) {
                     respuestas.add(new RespuestaAlumno(p, null));
+                    preguntasLength++;
                 }
             }
+            
             temaIndex = 0;
             preguntaIndex = 0;
+
+
+            chrExamen.setBase(SystemClock.elapsedRealtime());
+            chrExamen.start();
             setPregunta();
         } catch (Exception ex) {
         }
     }
+
 
     private void setPregunta() {
 
@@ -82,20 +99,28 @@ public class ExamenActivity extends AppCompatActivity  {
 
         Respuesta seleccionada = respuestas.get(preguntaIndex).getRespuestaSeleccionada();
         rgRespuestasExamen.clearCheck();
-        if(seleccionada == p.getRespuestas().get(0)){
+        if (seleccionada == p.getRespuestas().get(0)) {
             rbRespuestaExamen1.setChecked(true);
         }
-        if(seleccionada == p.getRespuestas().get(1)){
+        if (seleccionada == p.getRespuestas().get(1)) {
             rbRespuestaExamen2.setChecked(true);
         }
-        if(seleccionada == p.getRespuestas().get(2)){
+        if (seleccionada == p.getRespuestas().get(2)) {
             rbRespuestaExamen3.setChecked(true);
         }
+
+        tvStepExamen.setText(preguntaIndex + " / " + preguntasLength);
 
     }
 
     public void siguiente(View view) {
         seleccionarRespuesta();
+
+        if (preguntaIndex == materia.getTemas().get(temaIndex).getPreguntas().size() - 1 && temaIndex < materia.getTemas().size()) {
+            temaIndex++;
+            preguntaIndex = 0;
+        }
+
         if (preguntaIndex < materia.getTemas().get(temaIndex).getPreguntas().size()) {
             preguntaIndex++;
             setPregunta();
@@ -104,13 +129,17 @@ public class ExamenActivity extends AppCompatActivity  {
 
     public void anterior(View view) {
         seleccionarRespuesta();
+        if (preguntaIndex == 0 && temaIndex > 0) {
+            temaIndex--;
+            preguntaIndex = materia.getTemas().get(temaIndex).getPreguntas().size() - 1;
+        }
         if (preguntaIndex > 0) {
             preguntaIndex--;
             setPregunta();
         }
     }
 
-    private void seleccionarRespuesta(){
+    private void seleccionarRespuesta() {
         if (rbRespuestaExamen1.isChecked()) {
             respuestas.get(preguntaIndex).setRespuestaSeleccionada(respuestas.get(preguntaIndex).getPregunta().getRespuestas().get(0));
         }
@@ -121,7 +150,6 @@ public class ExamenActivity extends AppCompatActivity  {
             respuestas.get(preguntaIndex).setRespuestaSeleccionada(respuestas.get(preguntaIndex).getPregunta().getRespuestas().get(2));
         }
     }
-
 
 
 }
--- app/src/main/java/app2you/probateapp/controladores/Examen.java
+++ app/src/main/java/app2you/probateapp/controladores/Examen.java
@@ -5,16 +5,30 @@ import java.util.List;
 
 import app2you.probateapp.entidades.Materia;
 import app2you.probateapp.entidades.Pregunta;
+import app2you.probateapp.entidades.RespuestaAlumno;
+import app2you.probateapp.entidades.Tema;
 
 public class Examen {
     private Materia materia;
+    private List<RespuestaAlumno> respondidas = new ArrayList<>();
 
     public Examen(Materia materia) {
         this.materia = materia;
+        for (Tema tema: materia.getTemas()) {
+            for (Pregunta pregunta: tema.getPreguntas()) {
+                pregunta.setTema(tema);
+                respondidas.add(new RespuestaAlumno(pregunta, null));
+            }
+        }
     }
 
     public void iniciar(){
 
     }
+
+    public void finalizar(){
+
+    }
+
 
 }
