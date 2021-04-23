98aeed1708a1b39d97d3029f74bdb8ae2c7bdf73
Alexis Romero <romeroalexis1994@gmail.com>
2021-04-22 22:06:10 -0300

    ResultadoExamenActivity ExpandibleListView

--- app/src/main/java/app2you/probateapp/ResultadoExamenActivity.java
+++ app/src/main/java/app2you/probateapp/ResultadoExamenActivity.java
@@ -6,12 +6,17 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ExpandableListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import app2you.probateapp.controladores.Examen;
 import app2you.probateapp.entidades.Materia;
@@ -20,12 +25,29 @@ import app2you.probateapp.entidades.Respuesta;
 
 
 public class ResultadoExamenActivity extends AppCompatActivity {
-
+    ExpandableListView elvCorreccion;
+    List<String> listGroup = new ArrayList<>();
+    HashMap<String, ArrayList<String>> listChild = new HashMap<>();
+    ResultadoExamenAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado_examen);
+
+        elvCorreccion = findViewById(R.id.elvCorreccion);
+
+        for (int g = 0; g<= 10; g++) {
+            listGroup.add("Group " + g);
+            ArrayList<String> arrayList = new ArrayList<>();
+            for (int c= 0; c <= 5; c++){
+                arrayList.add("Item " + c);
+            }
+            listChild.put(listGroup.get(g), arrayList);
+        }
+
+        adapter = new ResultadoExamenAdapter(listGroup, listChild);
+        elvCorreccion.setAdapter(adapter);
 
     }
 
